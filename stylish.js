0d4c66cf7532994a0897f4ca64973415bc221de7
Brad Robinson <contact@toptensoftware.com>
2026-07-07 09:17:27 +1000

    Reworked for use as npm package

--- stylish.js
+++ stylish.js
@@ -1,84 +1,101 @@
-if (!window.stylish)
-    window.stylish = new EventTarget();
+import { Style } from "@codeonlyjs/core";
+
+const D = 'dark';
+const A = 'auto';
+const L = 'light';
 
 
-(function()
+class Stylish extends EventTarget
 {
-    let D = 'dark';
-    let A = 'auto';
-    let L = 'light';
-
-    // Load a theme - either D, L or A
-    window.stylish.loadTheme = function(name)
+    constructor()
     {
+        super();
+
+        // Listen for preferred theme changes
+        this.#prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+        this.#prefersDark.addEventListener('change', event => {
+            this.selectedTheme = A;
+        });
+    }
+
+    #prefersDark;
+
+    // Set the current theme
+    set selectedTheme(name)
+    {
+        // Detect change
+        let wasDarkMode = this.darkMode;
+
+        // Update classes
         let cl = document.documentElement.classList;
         cl.remove(D, L);
         if (name != A)
             cl.add(name);
-    };
 
-    // Query preferred theme
-    let prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-    // Listen for preferred theme changes
-    prefersDark.addEventListener('change', event => {
-        let wasDarkMode = window.stylish.darkMode;
-        preferredTheme = prefersDark.matches ? D : L;
-        selectedTheme = A;
-        setTheme();
-        window.stylish.setThemeSwitches();
-        if (wasDarkMode != window.stylish.darkMode)
-            fireDarkModeChange();
-    });
-
-    // Retrieve the currently selected theme
-    let selectedTheme = window.localStorage.getItem("stylish-theme") ?? A;
-    let preferredTheme = prefersDark.matches ? D : L;
-    if (preferredTheme == selectedTheme)
-        selectedTheme = "auto";
-
-    setTheme();
-
-    // Check/uncheck all ".theme-switch"s
-    function setTheme()
-    {
-        window.stylish.loadTheme(selectedTheme);
-        window.localStorage.setItem("stylish-theme", selectedTheme);
-    }
-
-    // Set all theme switches
-    window.stylish.setThemeSwitches = function()
-    {
+        // Update all theme switches
         document.querySelectorAll("input[type=checkbox].theme-switch").forEach(x => {
-            x.checked = window.stylish.darkMode
+            x.checked = this.darkMode
         });
-    }
 
-    Object.defineProperty(window.stylish, "darkMode", {
-        get() {
-            return selectedTheme == D || 
-                    (selectedTheme == A && preferredTheme == D);
+        // Changed?
+        if (wasDarkMode != this.darkMode)
+        {
+            // Fire change
+            let ev = new Event("darkModeChanged");
+            ev.darkMode = this.darkMode;
+            this.dispatchEvent(ev);
         }
-    });
 
-    function fireDarkModeChange()
-    {
-        let ev = new Event("darkModeChanged");
-        ev.darkMode = window.stylish.darkMode;
-        window.stylish.dispatchEvent(ev);
-    }
-
-
-    window.stylish.toggleTheme = function()
-    {
-        if (selectedTheme == A)
-            selectedTheme = preferredTheme == D ? L : D;
+        // Store it
+        // NB: this is read back by small script snippet that should be added to the 
+        //     head block of your index.html
+        /*
+        <script>
+        const theme = window.localStorage.getItem("stylish-theme");
+        if (theme) document.documentElement.classList.add(theme);
+        </script>
+        */
+        if (name == A)
+            window.localStorage.removeItem("stylish-theme")
         else
-            selectedTheme = A;
-        setTheme();
-        window.stylish.setThemeSwitches();
-        fireDarkModeChange();
+            window.localStorage.setItem("stylish-theme", name);
+
     }
 
+    // Get the current theme
+    get selectedTheme()
+    {
+        let cl = document.documentElement.classList;
+        if (cl.contains(D))
+            return D;
+        if (cl.contains(L))
+            return L;
+        return A;
+    }
 
-})();
+    // Get if current theme resolves to dark
+    get darkMode()
+    {
+        switch (this.selectedTheme)
+        {
+            case D:
+                return true;
+
+            case A:
+                return this.#prefersDark.matches;
+        }
+        return false;
+    }
+
+    // Toggle selected theme
+    toggleTheme()
+    {
+        if (this.selectedTheme == A)
+            this.selectedTheme = this.#prefersDark.matches ? L : D;
+        else
+            this.selectedTheme = A;
+    }
+
+}
+
+export let stylish = new Stylish();;
